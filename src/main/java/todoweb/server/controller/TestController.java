3084625a598cae76b6600b0cd5b527882b6adddd
Eunjin Kim <jennyeunjin@khu.ac.kr>
2023-12-24 22:13:05 +0900

    :sparkles: Feat: 테스트용 api 수정

--- src/main/java/todoweb/server/controller/TestController.java
+++ src/main/java/todoweb/server/controller/TestController.java
@@ -2,11 +2,13 @@ package todoweb.server.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import todoweb.server.payload.ApiResponse;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class TestController {
 
     @GetMapping("/version")
